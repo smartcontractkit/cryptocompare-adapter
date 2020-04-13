eed80b7328a4325c93b050b315f45f4714c04d3b
Thomas Hodges <me@thodges.com>
2020-04-12 19:25:11 -0500

    No defaults

--- index.js
+++ index.js
@@ -45,8 +45,8 @@ const createRequest = (input, callback) => {
     url = `https://min-api.cryptocompare.com/data/${endpoint}`
   }
 
-  const coin = input.data.from || input.data.coin || input.data.fsym || 'ETH'
-  const market = input.data.to || input.data.market || input.data.tsyms || 'USD'
+  const coin = input.data.from || input.data.coin || input.data.fsym || ''
+  const market = input.data.to || input.data.market || input.data.tsyms || ''
 
   const queryObj = {
     fsym: coin,
