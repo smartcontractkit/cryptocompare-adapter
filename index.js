993d216beb993d1b23e605bfdb70063617c1272f
Thomas Hodges <me@thodges.com>
2020-04-14 14:57:42 -0500

    Allow for customError and customParams to be specified

--- index.js
+++ index.js
@@ -1,120 +1,75 @@
-const rp = require('request-promise')
+const { Validator, requestRetry, getResult } = require('./adapter')
 const retries = process.env.RETRIES || 3
-const delay = process.env.RETRY_DELAY || 1000
 const timeout = process.env.TIMEOUT || 1000
 
-class ValidationError extends Error {
-  constructor (message) {
-    super(message)
-    this.name = 'ValidationError'
-    this.message = message
-  }
-
-  toJSON () {
-    return {
-      error: {
-        name: this.name,
-        message: this.message,
-        stacktrace: this.stack
-      }
-    }
-  }
+// Define custom error scenarios for the API.
+// Return true for the adapter to retry.
+const customError = (body) => {
+  if (body.Response === 'Error') return true
+  return false
 }
 
-const requestRetry = (options, retries) => {
-  return new Promise((resolve, reject) => {
-    const retry = (options, n) => {
-      return rp(options)
-        .then(response => {
-          if (response.body.error || response.body.Response === 'Error') {
-            if (n === 1) {
-              reject(response.body)
-            } else {
-              setTimeout(() => {
-                retries--
-                retry(options, retries)
-              }, delay)
-            }
-          } else {
-            return resolve(response)
-          }
-        })
-        .catch(error => {
-          if (n === 1) {
-            reject(error.body)
-          } else {
-            setTimeout(() => {
-              retries--
-              retry(options, retries)
-            }, delay)
-          }
-        })
-    }
-    return retry(options, retries)
-  })
-}
-
-const validateInput = (input) => {
-  return new Promise((resolve, reject) => {
-    if (typeof input.id === 'undefined') {
-      input.id = '1'
-    }
-    if (typeof input.data === 'undefined') {
-      reject(new ValidationError('No data supplied'))
-    }
-    resolve(input)
-  })
+// Define custom parameters to be used by the adapter.
+// Extra parameters can be stated in the extra object,
+// with a Boolean value indicating whether or not they
+// should be required.
+const customParams = {
+  base: ['fsym'],
+  quote: ['tsyms'],
+  extra: {
+    endpoint: false
+  }
 }
 
 const createRequest = (input, callback) => {
-  validateInput(input)
-    .then(input => {
-      const endpoint = input.data.endpoint || 'price'
-      const url = `https://min-api.cryptocompare.com/data/${endpoint}`
-      const coin = input.data.base || input.data.from || input.data.coin || input.data.fsym || ''
-      const market = input.data.quote || input.data.to || input.data.market || input.data.tsyms || ''
+  let validator
+  try {
+    validator = new Validator(input, customParams)
+  } catch (error) {
+    callback(500, {
+      jobRunID: input.id,
+      status: 'errored',
+      error,
+      statusCode: 500
+    })
+  }
+  const jobRunID = validator.validated.id
+  const endpoint = validator.validated.data.endpoint || 'price'
+  const url = `https://min-api.cryptocompare.com/data/${endpoint}`
+  const base = validator.validated.data.base.toUpperCase()
+  const quote = validator.validated.data.quote.toUpperCase()
 
-      const queryObj = {
-        fsym: coin,
-        fsyms: coin,
-        tsym: market,
-        tsyms: market
-      }
+  const queryObj = {
+    fsym: base,
+    fsyms: base,
+    tsym: quote,
+    tsyms: quote
+  }
 
-      const options = {
-        uri: url,
-        qs: queryObj,
-        json: true,
-        timeout,
-        resolveWithFullResponse: true
-      }
+  const options = {
+    uri: url,
+    qs: queryObj,
+    json: true,
+    timeout,
+    resolveWithFullResponse: true
+  }
 
-      requestRetry(options, retries)
-        .then(response => {
-          const result = response.body[market.toUpperCase()]
-          if (Number(result) === 0) throw new ValidationError('Zero result')
-          response.body.result = result
-          callback(response.statusCode, {
-            jobRunID: input.id,
-            data: response.body,
-            result,
-            statusCode: response.statusCode
-          })
-        })
-        .catch(error => {
-          callback(500, {
-            jobRunID: input.id,
-            status: 'errored',
-            error,
-            statusCode: 500
-          })
-        })
+  requestRetry(options, retries, customError)
+    .then(response => {
+      const result = getResult(response.body, [quote])
+      response.body.result = result
+      callback(response.statusCode, {
+        jobRunID,
+        data: response.body,
+        result,
+        statusCode: response.statusCode
+      })
     })
     .catch(error => {
       callback(500, {
-        jobRunID: input.id,
+        jobRunID,
         status: 'errored',
-        error: error.message,
+        error,
         statusCode: 500
       })
     })
